61390e6ca1c6f3f3aff7cb2edb5ca43a41fafd01
M A S T E R <kiril.svetoslavov.kirilov@gmail.com>
2023-02-11 11:41:52 +0200

    Initial commit

---
+++ src/main/java/bg/softuni/pathfinderworkshop/PathfinderWorkShopApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.pathfinderworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderWorkShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathfinderWorkShopApplication.class, args);
+    }
+
+}
